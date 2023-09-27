4942e56dfb0ebe68432b686e3f076eeddae6939c
Melozzo <carmecamma1995@gmail.com>
2023-09-27 15:27:58 +0200

    [edit]: vue components

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 //1. destrutturare il  metodo
-const {createApp}  = Vue
+const {createApp}  = Vue;
 
 //2. montare il metodo 
 createApp({
-
+  date(){
+    return{
+      saluto: 'Ciao vue'
+    }
+  }
 }).mount('#app')
+
